e2cc0ed307446a7d97eb1880f9f854c7c55a9ab7
Linus Rundberg Streuli <schizoakustik@schizoakustik.se>
2022-09-20 11:21:10 +0200

    adds ringtones

--- main.ts
+++ main.ts
@@ -1,7 +1,30 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         basic.showIcon(IconNames.Happy)
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
     } else if (receivedNumber == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+    } else {
+        basic.showIcon(IconNames.Sad)
+        music.startMelody(music.builtInMelody(Melodies.Funeral), MelodyOptions.Once)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (emoji > 1) {
+        emoji += -1
+    } else {
+        emoji = 3
+    }
+    if (emoji == 1) {
+        basic.showIcon(IconNames.Happy)
+    } else if (emoji == 2) {
         basic.showLeds(`
             . . . . .
             . # . # .
@@ -12,17 +35,11 @@ radio.onReceivedNumber(function (receivedNumber) {
     } else {
         basic.showIcon(IconNames.Sad)
     }
-    music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(emoji)
 })
 input.onButtonPressed(Button.B, function () {
-    if (emoji < 3) {
-        emoji += 1
-    } else {
-        emoji = 1
-    }
     if (emoji < 3) {
         emoji += 1
     } else {
